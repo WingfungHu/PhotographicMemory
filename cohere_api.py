f83de188e1c2bd4d03db00c8612cddfaf5f0921b
dogyu lee <dot.lee@mail.utoronto.ca>
2024-01-27 13:28:27 -0500

    Added functions

--- cohere_api.py
+++ cohere_api.py
@@ -13,12 +13,13 @@ class Chatbot():
     """
   For cohere calls
   """
+
     def __int__(self):
-        conversation = []
         image_description = ""
         generate_prompt = "Using the description provided, craft one question following an empahthizing comment to help draw out the nostalgic memories associated with this image. These questions are designed to be conversational and non-intrusive, intending to encourage the individual to reflect and share at their own pace, so refer to the past. Do not be too open ended or add explanations of why you generated that text. Description for photo: "
+        chat_prompt = 'Continue the conversation naturally connecting from the chat_history'
 
-    def generate(self, generate_prompt: str, image_description: str, conversation: list) -> str:
+    def generate(self, generate_prompt: str, image_description: str, conversation: list) -> dict:
 
         response = co.generate(
             model='command',
@@ -29,30 +30,17 @@ class Chatbot():
             stop_sequences=[],
             return_likelihoods='NONE')
 
-        conversation.append({"role": "Chatbot", "message": response[0].text})
+        return {"role": "Chatbot", "message": response[0].text}
 
-        return response[0].text
+    def chat(self, conversation: list, chat_prompt: str) -> dict:
 
+        chat = co.chat(
+            model='command',
+            message=chat_prompt,
+            temperature=0.3,
+            chat_history=conversation,
+            connectors=[],
+            documents=[]
+        )
 
-    user_input = "It was for halloween! Lot's of fun!"
-
-    conversation.append({"role": "User", "message": user_input})
-
-    history = 'Continue the conversation naturally connecting from the chat_history'
-
-    print(conversation)
-
-    chat = co.chat(
-        model='command',
-        message=history,
-        temperature=0.3,
-        chat_history=conversation,
-        connectors=[],
-        documents=[]
-    )
-
-    conversation.append({"role": "Chatbot", "message": chat.text})
-
-    # until user_input == "end chat" => while loop
-
-    print(conversation)
+        return {"role": "Chatbot", "message": chat.text}
