61e01b46ccdef4109f056a7214b5fe2be6a32e0f
Harjevan <hgill322@uwo.ca>
2024-01-27 14:03:05 -0500

    update on tts api

--- tts_api.py
+++ tts_api.py
@@ -2,8 +2,31 @@
 Date: Saturday, January 27 2024
 Description: recieves the generated text and converts it to speech
 '''
+import azure.cognitiveservices.speech as speechsdk
+from pydub.playback import play
+from pydub import AudioSegment
+import os
 
 class tts_api:
-    
+    #The subscription key and service region needed to access the service
+    subscription_key = 'e370f7cc63b042f2a1dd21387391bc2d'
+    service_region = 'https://eastus.api.cognitive.microsoft.com/'
+
+    #creates access to the service
     def __init__(self):
-        return
+        self.speech_config = speechsdk.SpeechConfig(subscription=self.subscription_key,region=self.service_region)
+
+    def convert_to_speech(self,text, filename = "output_audio.wav"):
+        #Audio config that specifies output audio file 
+        # ***** check to see if you have to create that file or if it creates itself *****
+        audio_config = speechsdk.audio.AudioOutputConfig(filename=filename)
+
+        #creates speech synthesizer with specified audio config
+        speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=self.speech_config, audio_config=audio_config)
+
+        result = speech_synthesizer.speak_text_async(text).get()
+
+
+
+
+        
