7cc4d21c8e789aa9fc9067530f7f5ac0f2c47801
Harjevan <hgill322@uwo.ca>
2024-01-27 12:51:54 -0500

    made into class format

--- Image_API.py
+++ Image_API.py
@@ -8,31 +8,49 @@ Desc: This is the code designed with instructions to
 '''
 
 import requests 
-        
+
+subscription_key = None
+base_url = None
+response = None
+
+class Image_API:
     
-def get_image_description(self,url):
-    # subscription key is what allows us to access the Azure Computer Vision Serive
     subscription_key = 'b2fce44dfd2b448c915051dcdec5426e'
-    assert subscription_key
-
     base_url = 'https://uofthacks11.cognitiveservices.azure.com//vision/v3.1/describe'
-    headers = {'Ocp-Apim-Subscription-Key': subscription_key, 'Content-Type': 'application/json'}
-    params = {
-        'language': 'en',  
-        'maxCandidates': '1',
-    }
-    #The url code for the picture, edit this so it can be changed accordingly
-    body = {
-        'url': url
-    }
-    response = requests.post(base_url, headers=headers, params=params, json=body)
-    response.raise_for_status()
-    analysis = response.json()
-    return analysis['description']
 
-    #print(analysis['description']['tags']) 
+    def __init__(self):
+        # empty
+        return
 
-    ''' 
-    image_tags = ' '.join(tag for tag in analysis['description']['tags'])
-    print ('Image Tags: ',image_tags)
-    '''
+    # def set_environement(self):
+    #     subscription_key = 'b2fce44dfd2b448c915051dcdec5426e'
+    #     base_url = 'https://uofthacks11.cognitiveservices.azure.com//vision/v3.1/describe'
+    #     analysis = ' '
+
+    def get_image_description(self,url):
+        # subscription key is what allows us to access the Azure Computer Vision Serive
+        # subscription_key = 'b2fce44dfd2b448c915051dcdec5426e'
+        # assert subscription_key
+
+        #This is the endpoint
+        # base_url = 'https://uofthacks11.cognitiveservices.azure.com//vision/v3.1/describe'
+        headers = {'Ocp-Apim-Subscription-Key': self.subscription_key, 'Content-Type': 'application/json'}
+        params = {
+            'language': 'en',  #Language is set to english
+            'maxCandidates': '1',
+        }
+        #The url code for the picture, recieves the url from the 
+        body = {
+            'url': url
+        }
+        response = requests.post(self.base_url, headers=headers, params=params, json=body)
+        response.raise_for_status()
+        analysis = response.json()
+        return analysis['description']
+
+        #print(analysis['description']['tags']) 
+
+        ''' 
+        image_tags = ' '.join(tag for tag in analysis['description']['tags'])
+        print ('Image Tags: ',image_tags)
+        '''
