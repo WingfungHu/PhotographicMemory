69e44c355ce6f8a1adeff8c268175d711d6a3800
Harjevan <hgill322@uwo.ca>
2024-01-27 15:38:39 -0500

    uploading a new version

--- tts_api.py
+++ tts_api.py
@@ -6,6 +6,10 @@ import azure.cognitiveservices.speech as speechsdk
 from pydub.playback import play
 from pydub import AudioSegment
 import os
+from flask import Flask, request, send_file#NEW
+from flask_cors import CORS
+
+app = Flask(__name__)#NEW
 
 class tts_api:
     #The subscription key and service region needed to access the service
@@ -16,35 +20,71 @@ class tts_api:
     def __init__(self):
         self.speech_config = speechsdk.SpeechConfig(subscription=self.subscription_key,region=self.service_region)
 
-    def convert_to_speech(self,text, filename = "output_audio.wav"):
-        #Audio config that specifies output audio file 
-        # ***** check to see if you have to create that file or if it creates itself *****
-        audio_config = speechsdk.audio.AudioOutputConfig(filename=filename)
+@app.route('/convert', methods=['POST'])#NEW
+def convert_to_speech():
+    text = request.form['text']
+    tts_service = tts_api()
+    audio_file = tts_service.convert_to_speech(text)
 
-        #creates speech synthesizer with specified audio config
-        speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=self.speech_config, audio_config=audio_config)
+    if audio_file:
+        return send_file(audio_file, as_attachment=True)
+    else:
+        return "Conversion failed", 500
+    '''
+    #Audio config that specifies output audio file 
+    # ***** check to see if you have to create that file or if it creates itself *****
+    audio_config = speechsdk.audio.AudioOutputConfig(filename=filename)
 
-        #tts conversion
-        result = speech_synthesizer.speak_text_async(text).get()
+    #creates speech synthesizer with specified audio config
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=self.speech_config, audio_config=audio_config)
 
-        if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-            print ("Speech Synthesized to [{}]".format(filename))
-            return filename
-        elif result.reason == speechsdk.ResultReason.Canceled:
-            cancellation_details = result.cancellation_details
-            print ("Speech Synthesis cancelled: {}".format(cancellation_details.reason))
+    #tts conversion
+    result = speech_synthesizer.speak_text_async(text).get()
 
-        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            if cancellation_details.error_details:
-                print("Error details: {}".format(cancellation_details.error_details))
-        return None
-    
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        print ("Speech Synthesized to [{}]".format(filename))
+        return filename
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print ("Speech Synthesis cancelled: {}".format(cancellation_details.reason))
+
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        if cancellation_details.error_details:
+            print("Error details: {}".format(cancellation_details.error_details))
+    return None
+    '''
+    '''
+    #This method may need editing/replacing from
+    #Start
     def play_audio(self,filename):
         if filename and os.path.exists(filename):
             audio = AudioSegment.from_wav(filename)
             play(audio)
         else:
             print("Audio file not found.")
+    
+       # Test the TextToSpeechService class
+        def test_text_to_speech():
+            tts_service = tts_api()
+            
+            # Convert text to speech
+            audio_file = tts_service.convert_to_speech("Hello, this is a test of Microsoft's text to speech API.")
+
+            # Play the audio file
+            if audio_file:
+                tts_service.play_audio(audio_file)
+            else:
+                print("Failed to convert text to speech.")
+    #Finish       
+
+         # Run the test
+        test_text_to_speech()
+    '''
+#NEW    
+if __name__ == '__main__':
+    app.run(debug=True)   
+CORS(app)
+
 
 
 
