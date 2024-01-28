2fa9e4991556b015607577be61dc83c531a6aef8
Ray <yenray.wen@gmail.com>
2024-01-27 20:38:07 -0500

    fix typo crashing home page

--- homepage.py
+++ homepage.py
@@ -22,7 +22,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['GETgit', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     app.logger.debug('A value for debugging')
     global backend_object
